65a2c61a9b9b1eaf400fc787c6180724d573072b
Rafael Herrera Castillo <rhercas779a@ieslacampina.es>
2022-01-25 09:30:45 +0100

    primera

---
+++ INSERT.sql
@@ -0,0 +1,14 @@
+INSERT INTO USUARIO1 (nombre, apellidos, edad, dirección, email)
+VALUE(Cristian, Escobar Torres, 24, Calle Nueva, nº 50, cristianescobartorres@gmail.com);
+
+INSERT INTO AULA2 (nombre, numero, planta, descrpcion)
+VALUE (Musica, 120, 2, equipada con instrumentos);
+
+INSERT INTO CURSO3 (codigo, nombre, descrpcion, horas)
+VALUE (4158, ASIR, equipada con equipos informaticos, 34);
+
+INSERT INTO EDIFICIO4 (nombre, descrpcion)
+VALUE (edificio 2, es el mas nuevo y tiene 3 plantas, en el estan los grados superiores);
+
+INSERT INTO EDIFICIO4 (nombre, descrpcion)
+VALUE (edificio 1, es el mas nuevo y tiene 5 plantas, en el estan los grados superiores);
---
+++ UPDATE.sql
@@ -0,0 +1,14 @@
+UPDATE USUARIO1
+SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester'; 
+
+UPDATE AULA2
+SET Nombre = 'Aula', Numero= '9', Planta = '1', Descripción = 'Inglés';
+
+UPDATE CURSO3
+SET Código = '99', Nombre= 'Matemáticas', Matemáticas académicas. = '50', Horas = '100';
+
+UPDATE EDIFICIO4
+SET Nombre = 'Empires States', Descripción= 'Muy alto';
+
+UPDATE USUARIO10
+SET Nombre = 'Juan', Apellido= 'Manuel', Edad = '19', Dirección = 'Sevilla';
